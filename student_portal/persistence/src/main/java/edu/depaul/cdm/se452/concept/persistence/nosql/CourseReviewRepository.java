f4f1554b6816bd2a669273d0510b7f11bfe2f3ee
Nicole Emich <90917673+nve3@users.noreply.github.com>
2021-10-12 22:41:34 -0500

    Delete student_portal directory

--- student_portal/persistence/src/main/java/edu/depaul/cdm/se452/concept/persistence/nosql/CourseReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.depaul.cdm.se452.concept.persistence.nosql;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CourseReviewRepository extends MongoRepository<CourseReview, String> {
-
-}
--- student_portal/persistence/src/main/java/edu/depaul/cdm/se452/concept/persistence/relational/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.depaul.cdm.se452.concept.persistence.relational;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-/**
- * For Pagination and Other JPA functionality beyond base CRUD services
- * @see https://docs.spring.io/spring-data/data-jpa/docs/current/api/org/springframework/data/jpa/repository/JpaRepository.html
- */
-public interface StudentRepository extends JpaRepository<Student, Long>  {
-}
