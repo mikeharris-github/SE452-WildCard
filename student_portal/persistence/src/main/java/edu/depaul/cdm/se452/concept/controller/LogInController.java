59651da4929b425df5452755834cdae821e4ae37
Nicole Emich <nemich@depaul.edu>
2021-11-05 17:30:15 -0500

    a bunch of updates to get the login page to work on local host

---
+++ student_portal/persistence/src/main/java/edu/depaul/cdm/se452/concept/controller/LogInController.java
@@ -0,0 +1,21 @@
+package edu.depaul.cdm.se452.concept.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LogInController {
+    @Value( "${spring.profiles.active}" )
+    private String login;
+
+    @GetMapping("/")
+    public ModelAndView showGreeting() {
+        //uses the name of the HTML file for MV
+        ModelAndView mv = new ModelAndView("login");
+        mv.addObject("name" , "Hello SE352/452 from Group 7 " + login);
+        return mv;
+    }
+
+}
