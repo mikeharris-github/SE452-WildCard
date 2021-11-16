c9607d895aad09849aab38aea1cdbe0aa9e81390
Mike Harris <mikeharris.mailbox@gmail.com>
2021-11-16 10:45:10 -0600

    CourseCatalogController, CourseCatalog java and html page updates

--- student_portal/persistence/src/main/java/edu/depaul/cdm/se452/concept/controller/CourseCatalogController.java
+++ student_portal/persistence/src/main/java/edu/depaul/cdm/se452/concept/controller/CourseCatalogController.java
@@ -1,14 +1,34 @@
 package edu.depaul.cdm.se452.concept.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.depaul.cdm.se452.concept.model.CourseCatalog;
+import edu.depaul.cdm.se452.concept.model.CourseCatalogRepository;
+
 @Controller
+@RequestMapping("/coursecatalog")
 public class CourseCatalogController {
-    @GetMapping("/coursecatalog")
-    public ModelAndView showGreeting() {
-        ModelAndView mv = new ModelAndView("coursecatalog");
-        // mv.addObject("name", "Hello SE352/452 from test");
-        return mv;
-    } 
+    private CourseCatalogRepository courseCatRepo;
+
+    public CourseCatalogController(CourseCatalogRepository courseCatRepo){
+        this.courseCatRepo = courseCatRepo;
+    }
+
+    @GetMapping
+    public String showCourseCatalog(Model model) {
+        Iterable<CourseCatalog> courses = courseCatRepo.findAll();
+        courses.forEach(courseItem -> System.out.println(courseItem.toString()));
+        model.addAttribute("courseItem", courses);
+        return "courseCatalog";
+      }
+
+    // @GetMapping("/coursecatalog")
+    // public ModelAndView showGreeting() {
+    //     ModelAndView mv = new ModelAndView("coursecatalog");
+    //     // mv.addObject("name", "Hello SE352/452 from test");
+    //     return mv;
+    // } 
 }
--- student_portal/persistence/src/main/java/edu/depaul/cdm/se452/concept/model/CourseCatalog.java
+++ student_portal/persistence/src/main/java/edu/depaul/cdm/se452/concept/model/CourseCatalog.java
@@ -22,4 +22,106 @@ public class CourseCatalog {
 	private String endDate;
 	private int availableSeats;
 	private String term;
+
+
+
+    /**
+     * @return long return the id
+     */
+    public String getCourseName() {
+        return courseName;
+    }
+
+    /**
+     * @param courseName the id to set
+     */
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getCourseID() {
+        return courseID;
+    }
+
+    /**
+     * @param courseID the name to set
+     */
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    /**
+     * @return String return the dept
+     */
+    public String getDepartment() {
+        return department;
+    }
+
+    /**
+     * @param dept the dept to set
+     */
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    /**
+     * @return int return the num
+     */
+    public String getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * @param startDate the num to set
+     */
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    /**
+     * @return String return the term
+     */
+    public String getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * @param endDate the term to set
+     */
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    /**
+     * @return char return the grade
+     */
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    /**
+     * @param availableSeats the grade to set
+     */
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    /**
+     * @return int return the units
+     */
+    public String getTerm() {
+        return term;
+    }
+
+    /**
+     * @param units the units to set
+     */
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+
 }
